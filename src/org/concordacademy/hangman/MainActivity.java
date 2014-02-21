64ed9b04d0ca6b9c87af3454fa176264eab06119
Will <william.harringto@concordacademy.org>
2014-02-20 20:34:31 -0500

    Just Formatting, couldn't fix it
    Mine isn't even launching. In fact it isn't even installing on my
    device, despite what the console says

--- src/org/concordacademy/hangman/MainActivity.java
+++ src/org/concordacademy/hangman/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	private final String MA = "[Main Activity]";
+	private final String MA = "Main Activity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,52 +17,44 @@ public class MainActivity extends Activity {
 		Log.i(MA, "Loading Title Screen.");
 	}
 
-	
 	// This Below Starts the Intent Push to the "Play Screen"
 
 	public void playGame(View v) {
 		Log.i(MA, "Starting Game");
 		Intent pushToPlayScreen = new Intent(this, PlayScreen.class);
 		startActivity(pushToPlayScreen);
-		
 
 	}
-	
-	@Override
-    public void onStart()
-    {
-    	super.onStart();
-    	Log.i(MA, "onStart");
-    	
-    }
-    
-    @Override
-    public void onResume() 
-    {
-    	super.onResume();
-    	Log.i(MA, "onResume");
-    }
-    
-    @Override
-    public void onPause() 
-    {
-    	super.onPause();
-    	Log.i(MA, "onPause");
-    }
-    
-    @Override
-    public void onStop() 
-    {
-    	super.onStop();
-    	Log.i(MA, "onStop");
-    }
-    
-    @Override
-    public void onDestroy() 
-    {
-    	super.onDestroy();
-    	Log.i(MA, "onDestroy");
-    }
 
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.i(MA, "onStart");
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.i(MA, "onResume");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.i(MA, "onPause");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.i(MA, "onStop");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(MA, "onDestroy");
+	}
 
 }
--- src/org/concordacademy/hangman/PlayScreen.java
+++ src/org/concordacademy/hangman/PlayScreen.java
@@ -5,17 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class PlayScreen extends Activity {
-	
-	//The String Below will tell Console/LogCat the processes of The PlayScreen Activity
-	
-	private final String PS = "[Play Screen]";
-	
+
+	// The String Below will tell Console/LogCat the processes of The PlayScreen Activity
+
+	private final String PS = "Play Screen";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playscreen);
 		Log.i(PS, "Loading Play Screen.");
 	}
-
 
 }
