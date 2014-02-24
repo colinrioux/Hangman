43c2f3f2410fa3f4db37fe489140cd729313106c
colin350 <colin350@gmail.com>
2014-02-24 14:08:31 -0500

    Input Reader of Text File

--- src/org/concordacademy/hangman/PlayScreen.java
+++ src/org/concordacademy/hangman/PlayScreen.java
@@ -1,5 +1,12 @@
 package org.concordacademy.hangman;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +22,35 @@ public class PlayScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playscreen);
 		Log.i(PS, "Loading Play Screen.");
+	}
+	
+	// Read Text File entitled wordsEn.txt 
+	public String readFromFile() {
+		String words = "";
+		
+		try { 
+			InputStream inputstream = openFileInput("wordsEn.txt");
+			if (inputstream != null) {
+				InputStreamReader inputStreamReader = new InputStreamReader(inputstream);
+				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+				String receiveString = "";
+				StringBuilder stringBuilder = new StringBuilder();
+				
+				while ( (receiveString = bufferedReader.readLine()) != null ) {
+	                stringBuilder.append(receiveString);
+	            }
+				inputstream.close();
+	            words = stringBuilder.toString();
+			}
+			
+		}
+		catch (FileNotFoundException e) {
+	        Log.e("login activity", "File not found: " + e.toString());
+	    } catch (IOException e) {
+	        Log.e("login activity", "Can not read file: " + e.toString());
+	    }
+
+	    return words;
 	}
 
 }
