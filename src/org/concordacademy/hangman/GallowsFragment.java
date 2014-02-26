9260023b9c4927079f63604efa0779c3db4af8a6
Will <william.harringto@concordacademy.org>
2014-02-26 11:35:32 -0500

    Added Gallows Fragment

---
+++ src/org/concordacademy/hangman/GallowsFragment.java
@@ -0,0 +1,22 @@
+package org.concordacademy.hangman;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GallowsFragment extends Fragment {
+
+	View view;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		view = inflater.inflate(R.layout.activity_gallows, container, false);
+		return view;
+
+	}
+
+}
