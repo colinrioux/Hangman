4cf71ed43049c908c129df6e2820e6dbb55139b4
Will <william.harringto@concordacademy.org>
2014-03-03 18:48:44 -0500

    Gallows Fragment stuff-very temporary pls dont edit yet
    I think I can actually do the gallows without a fragment but I would
    need to edit PlayScreen.java. If you're still working on that then tell
    me but it would be a lot easier without the Fragment.

--- gen/org/concordacademy/hangman/R.java
+++ gen/org/concordacademy/hangman/R.java
@@ -8,6 +8,10 @@
 package org.concordacademy.hangman;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_in=0x7f040000;
+        public static final int fade_out=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,19 +21,20 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_hangmanicon=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080004;
-        public static final int fragment1=0x7f080003;
-        public static final int keyBoard=0x7f080002;
-        public static final int testview1=0x7f080001;
+        public static final int LinearLayout1=0x7f090001;
+        public static final int action_settings=0x7f090005;
+        public static final int fragment1=0x7f090004;
+        public static final int imageSwitcher1=0x7f090000;
+        public static final int keyBoard=0x7f090003;
+        public static final int testview1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_gallows=0x7f030000;
@@ -37,15 +42,15 @@ public final class R {
         public static final int activity_playscreen=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int ButtonTitle=0x7f050004;
-        public static final int KeyBoard=0x7f050005;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title=0x7f050003;
+        public static final int ButtonTitle=0x7f060004;
+        public static final int KeyBoard=0x7f060005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title=0x7f060003;
     }
     public static final class style {
         /** 
@@ -69,10 +74,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
