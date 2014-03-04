c372498051a1d1c92cd04012531561a276d4c928
Will <william.harringto@concordacademy.org>
2014-03-03 19:20:19 -0500

    minor changes to gallows

--- gen/org/concordacademy/hangman/R.java
+++ gen/org/concordacademy/hangman/R.java
@@ -29,23 +29,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-<<<<<<< HEAD
         public static final int LinearLayout1=0x7f090001;
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090008;
+        public static final int displayedWord=0x7f090006;
         public static final int fragment1=0x7f090004;
-        public static final int imageSwitcher1=0x7f090000;
+        public static final int guessButton=0x7f090007;
+        public static final int imageView1=0x7f090000;
         public static final int keyBoard=0x7f090003;
         public static final int testview1=0x7f090002;
-=======
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080007;
-        public static final int displayedWord=0x7f080005;
-        public static final int fragment1=0x7f080003;
-        public static final int guessButton=0x7f080006;
-        public static final int keyBoard=0x7f080002;
-        public static final int testview1=0x7f080001;
-        public static final int textField=0x7f080004;
->>>>>>> 659af3f2852b6615bfe30cf34fd4b781866f601d
+        public static final int textField=0x7f090005;
     }
     public static final class layout {
         public static final int activity_gallows=0x7f030000;
